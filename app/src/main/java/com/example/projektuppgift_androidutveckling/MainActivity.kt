748cf602f73d78de9208b45db0fb6732f9d50dfe
markopaunovic1 <marpau970514@hotmail.com>
2023-02-06 13:06:25 +0100

    Connected buttons to alternative Activities

--- app/src/main/java/com/example/projektuppgift_androidutveckling/MainActivity.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.example.projektuppgift_androidutveckling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.firestore.auth.User
 
 class MainActivity : AppCompatActivity() {
+
+    //Create Accounts
+    private lateinit var CreatePrivateButton: Button
+    private lateinit var CreateCompanyButton: Button
+
+    //Login Accounts
+    private lateinit var LoginPrivateButton: Button
+    private lateinit var LoginCompanyButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        CreatePrivateButton = findViewById(R.id.createPrivButton)
+        CreateCompanyButton = findViewById(R.id.createCompButton)
 
+        LoginPrivateButton = findViewById(R.id.logPrivButton)
+        LoginCompanyButton = findViewById(R.id.logCompButton)
+
+        CreatePrivateButton.setOnClickListener {
+            val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
+        }
+
+        CreateCompanyButton.setOnClickListener {
+            val intent = Intent(this, CompanyActivity::class.java)
+            startActivity(intent)
+        }
+
+        LoginPrivateButton.setOnClickListener {
+            val intent = Intent(this, UserLogInActivity::class.java)
+            startActivity(intent)
+        }
+
+        LoginCompanyButton.setOnClickListener {
+            val intent = Intent(this, CompanyLogInActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
