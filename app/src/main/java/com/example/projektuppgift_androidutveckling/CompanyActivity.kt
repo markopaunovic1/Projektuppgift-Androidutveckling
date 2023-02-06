0ccb94b8d088685ee985810a24a9d7df12b5d9ac
friddler <yhma22.nilsfr@folkuniversitetet.nu>
2023-02-06 10:45:17 +0100

    created new activities

--- app/src/main/java/com/example/projektuppgift_androidutveckling/CompanyActivity.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/CompanyActivity.kt
@@ -7,5 +7,7 @@ class CompanyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_company)
+
+
     }
 }
