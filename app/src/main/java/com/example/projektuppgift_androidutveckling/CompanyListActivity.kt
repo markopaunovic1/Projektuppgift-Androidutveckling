0b7ae3a9c6eb939c146f6078c30f042ad5b9e437
mouhammadazroun <yhma22.azromo@folkuniversitetet.nu>
2023-02-13 22:03:50 +0100

    Upload restaurant data

--- app/src/main/java/com/example/projektuppgift_androidutveckling/CompanyListActivity.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/CompanyListActivity.kt
@@ -1,13 +1,20 @@
 package com.example.projektuppgift_androidutveckling
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CompanyListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_company_list)
 
+        val addItemButton = findViewById<FloatingActionButton>(R.id.addItemButton)
+        addItemButton.setOnClickListener{
+            val intent = Intent(this, AddFood:: class.java)
+            startActivity(intent)
+        }
 
     }
 }
