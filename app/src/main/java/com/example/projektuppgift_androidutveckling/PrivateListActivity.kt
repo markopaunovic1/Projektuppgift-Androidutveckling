42eb13f54175c44df54dcea6ffd6169e7b33165c
lizahjortling <lizhj90@outlook.com>
2023-02-14 21:40:08 +0100

    First working version of list of restaurant. Landing page takes you there

--- app/src/main/java/com/example/projektuppgift_androidutveckling/PrivateListActivity.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/PrivateListActivity.kt
@@ -2,12 +2,23 @@ package com.example.projektuppgift_androidutveckling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class PrivateListActivity : AppCompatActivity() {
+    lateinit var adapter: RecyclerAdapterRestaurante
+    lateinit var recycler : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.private_activity_list)
+        recycler = findViewById(R.id.RestauranteRecyclerView)
 
+        val restaurants = arrayListOf(Restaurante("SuperBurgerKing","ABCDV"),Restaurante("SuperSausageKing","ABCDV"))
+
+
+        adapter = RecyclerAdapterRestaurante(this,restaurants)
+        recycler.adapter = adapter;
+        recycler.layoutManager = LinearLayoutManager(this)
 
 
     }
---
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/RecyclerAdapterRestaurante.kt
@@ -0,0 +1,34 @@
+package com.example.projektuppgift_androidutveckling
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapterRestaurante(val context : Context, val restaurants : MutableList<Restaurante>)
+    : RecyclerView.Adapter<RecyclerAdapterRestaurante.viewHolder>(){
+    val layoutInflater = LayoutInflater.from(context)
+
+
+
+    inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameTextView = itemView.findViewById<TextView>(R.id.nameOfRestaurantTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val itemView = layoutInflater.inflate(R.layout.private_resturant_list, parent, false)
+        return viewHolder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return restaurants.size;
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        val restaurante = restaurants[position]
+        holder.nameTextView.text = restaurante.name
+    }
+}
