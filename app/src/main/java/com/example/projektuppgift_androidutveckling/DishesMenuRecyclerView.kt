a35346ee63ed3454977cc7b686ca06a5b54a2731
lizahjortling <lizhj90@outlook.com>
2023-02-21 11:49:43 +0100

    added dishesAktivity

---
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/DishesMenuRecyclerView.kt
@@ -0,0 +1,11 @@
+package com.example.projektuppgift_androidutveckling
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DishesMenuRecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dishes_menu_recycler_view)
+    }
+}
