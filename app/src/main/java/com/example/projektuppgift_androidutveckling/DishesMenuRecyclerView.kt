c56420ecefe10015fa0a38df5343f5a873811a73
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-02-22 15:32:23 +0100

    Added a menuButton to the landingpage so the navigationdrawer is easier to find.

--- app/src/main/java/com/example/projektuppgift_androidutveckling/DishesMenuRecyclerView.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/DishesMenuRecyclerView.kt
@@ -1,11 +1,21 @@
 package com.example.projektuppgift_androidutveckling
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class DishesMenuRecyclerView : AppCompatActivity() {
+    lateinit var db: FirebaseFirestore
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dishes_menu_recycler_view)
-    }
+
+        db = Firebase.firestore
+        auth = Firebase.auth
+}
 }
