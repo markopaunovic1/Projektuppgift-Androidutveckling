ae3fbb68413867b857bf847f4063a79c41930b8b
markopaunovic1 <marpau970514@hotmail.com>
2023-02-27 01:17:10 +0100

    Implemented favorite checkBox

--- app/src/main/java/com/example/projektuppgift_androidutveckling/User/RecyclerAdapterRestaurante.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/User/RecyclerAdapterRestaurante.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projektuppgift_androidutveckling.R
 import com.example.projektuppgift_androidutveckling.Restaurant
@@ -26,6 +28,15 @@ class RecyclerAdapterRestaurante(val context : Context, val restaurants : List<R
         val restaurante = restaurants[position]
         holder.nameTextView.text = restaurante.restaurantName
         holder.listItemPosition = position
+
+        holder.favoriteCheckBox.setOnCheckedChangeListener { checkBox , isChecked ->
+             if (isChecked) {
+                 Toast.makeText(context, "Added to favorite", Toast.LENGTH_LONG).show()
+             } else {
+                 Toast.makeText(context, "Removed from favorite", Toast.LENGTH_LONG).show()
+             }
+        }
+
     }
 
     override fun getItemCount(): Int {
@@ -34,6 +45,7 @@ class RecyclerAdapterRestaurante(val context : Context, val restaurants : List<R
 
     inner class viewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameTextView = itemView.findViewById<TextView>(R.id.nameOfRestaurantTextView)
+        val favoriteCheckBox = itemView.findViewById<CheckBox>(R.id.cb_Favorite)
         var listItemPosition = 0
 
         init {
