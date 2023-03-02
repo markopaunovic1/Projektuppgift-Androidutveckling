50bf6e6759b9b8eaf3a19c84cc59b772243395e9
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-03-02 12:08:36 +0100

    layout editing

--- app/src/main/java/com/example/projektuppgift_androidutveckling/User/SettingsActivity.kt
+++ app/src/main/java/com/example/projektuppgift_androidutveckling/User/SettingsActivity.kt
@@ -1,12 +1,22 @@
 package com.example.projektuppgift_androidutveckling.User
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.projektuppgift_androidutveckling.R
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+
+        val backButton = findViewById<ImageButton>(R.id.backButtonSettings)
+
+        backButton.setOnClickListener {
+            val intent = Intent(this, LandingPagePrivateActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
